fe411bbd7e4539518836f6145c0e3a1d5954c136
Cristopher <pca_1997_10@hotmail.com>
2024-05-04 00:13:45 -0500

    Cliente Terminado

---
+++ PrjFlowersshoesAPI/DAO/ClientesDAO.cs
@@ -0,0 +1,119 @@
+﻿using System.Data.SqlClient;
+using PrjFlowersshoesAPI.Models;
+namespace PrjFlowersshoesAPI.DAO
+{
+    public class ClientesDAO
+    {
+        public string cad_sql { get; set; } = string.Empty;
+
+        public ClientesDAO(IConfiguration cfg)
+        {
+            cad_sql = cfg.GetConnectionString("cn1");
+        }
+
+       public List<Clientes> GetClientes()
+        {
+            var list = new List<Clientes>();
+
+            SqlDataReader rd = SqlHelper.ExecuteReader(cad_sql,"PA_LISTAR_CLIENTES");
+            while (rd.Read())
+            {
+                list.Add(new Clientes()
+                {
+                    idcli = rd.GetInt32(0),
+                    nomcli = rd.GetString(1),
+                    apellidos=rd.GetString(2),
+                    tipodocumento= rd.GetString(3),
+                    nrodocumento= rd.GetString(4),
+                    telefono= rd.GetString(5),
+                    direccion= rd.GetString(6),
+                   estado = rd.GetString(7)
+                });             
+
+            }
+            rd.Close();
+            return list;
+        }
+
+        public string GrabarCliente(Clientes obj)
+        {
+            string mensaje = "";
+
+            List<KeyValuePair<string, object>> parametros = new List<KeyValuePair<string, object>>();
+            parametros.Add(new KeyValuePair<string, object>("@nomcli", obj.nomcli));
+            parametros.Add(new KeyValuePair<string, object>("@apellidos", obj.apellidos));
+            parametros.Add(new KeyValuePair<string, object>("@tipodocumento", obj.tipodocumento));
+            parametros.Add(new KeyValuePair<string, object>("@nrodocumento", obj.nrodocumento));
+            parametros.Add(new KeyValuePair<string, object>("@telefono", obj.telefono));
+            parametros.Add(new KeyValuePair<string, object>("@direccion", obj.direccion));
+           
+
+            try
+            {
+                mensaje = SqlHelper.ExecuteNonQuery2(cad_sql, "PA_GRABAR_CLIENTE", parametros);
+            }
+            catch (Exception ex)
+            {
+
+                mensaje = ex.Message;
+            }
+            return mensaje;
+        }
+
+        public string ActualizarCliente(Clientes obj)
+        {
+            string mensaje = "";
+
+            List<KeyValuePair<string, object>> parametros = new List<KeyValuePair<string, object>>();
+            parametros.Add(new KeyValuePair<string, object>("@idcli", obj.idcli));
+            parametros.Add(new KeyValuePair<string, object>("@nomcli", obj.nomcli));
+            parametros.Add(new KeyValuePair<string, object>("@apellidos", obj.apellidos));
+            parametros.Add(new KeyValuePair<string, object>("@tipodocumento", obj.tipodocumento));
+            parametros.Add(new KeyValuePair<string, object>("@nrodocumento", obj.nrodocumento));
+            parametros.Add(new KeyValuePair<string, object>("@telefono", obj.telefono));
+            parametros.Add(new KeyValuePair<string, object>("@direccion", obj.direccion));
+            try
+            {
+                mensaje = SqlHelper.ExecuteNonQuery2(cad_sql, "PA_MODIFICAR_CLIENTE", parametros);
+            }
+            catch (Exception ex)
+            {
+
+                mensaje = ex.Message;
+            }
+            return mensaje;
+        }
+
+        public string EliminarClientes(int id)
+        {
+            string mensaje = "";           
+            try
+            {
+                SqlHelper.ExecuteNonQuery(cad_sql,
+                    "PA_ELIMINAR_CLIENTES", id);
+                mensaje = $"Se Eliminó correctamente al Cliente: {id}";
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+            return mensaje;
+        }
+
+        public string RestaurarClientes(int id)
+        {
+            string mensaje = "";
+            try
+            {
+                SqlHelper.ExecuteNonQuery(cad_sql,
+                    "PA_RESTAURAR_CLIENTES", id);
+                mensaje = $"Se Restauro correctamente al Cliente: {id}";
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+            return mensaje;
+        }
+    }
+}
