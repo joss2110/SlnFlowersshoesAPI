6f725a4b033974ae89f03110d2b29ea3a799229d
MAJO-05 <mariajosegaspar95@gmail.com>
2024-05-07 12:13:46 -0500

    LISTO

--- PrjFlowersshoesAPI/Models/PA_LISTAR_STOCKS.cs
+++ PrjFlowersshoesAPI/Models/PA_LISTAR_STOCKS.cs
@@ -3,7 +3,7 @@
     public class PA_LISTAR_STOCKS
     {
         public int idstock { get; set; }
-        public string nompro { get; set; }
+        public string nompro { get; set; } 
         public string imagen { get; set; }
         public string color { get; set; }
         public string talla { get; set; }
