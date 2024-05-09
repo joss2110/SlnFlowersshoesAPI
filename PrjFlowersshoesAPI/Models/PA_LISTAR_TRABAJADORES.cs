9da8e2322f5f6719f5595e3c46be71d3f54dabc1
Jirekcz <Jirekcz03ft@gmail.com>
2024-05-09 15:37:19 -0500

    prueba

--- PrjFlowersshoesAPI/Models/PA_LISTAR_TRABAJADORES.cs
+++ PrjFlowersshoesAPI/Models/PA_LISTAR_TRABAJADORES.cs
@@ -8,7 +8,7 @@
         public string nroDocumento { get; set; } = String.Empty;
         public string direccion { get; set; } = String.Empty;
         public string email { get; set; } = String.Empty;
-        public string nomRol { get; set; }
+        public string nomRol { get; set; } = String.Empty;
         public string estado { get; set; } = String.Empty;
     }
 }
